1cc93e873158f6b19772ab78cbeebe2aa0bc46f8
bouali.kamel <bouali.kamel@elit.dz>
2021-01-11 11:08:04 +0100

    remove some additional library

--- src/layouts/header/TabPanels.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-import { Paper, Tabs, Tab } from "@material-ui/core";
-
-export default function TabPanels() {
-  return (<div className='menuWidget'></div>);
-}
